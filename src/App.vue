b46d545ed2c8d9fdee959c8d08f07655de55cc61
Mario Arriola <mario.arriola@commercetools.com>
2024-05-23 17:17:56 +0200

    chore added btn

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Btn.vue'
 </script>
 
 <template>
@@ -11,7 +12,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+ 
   <HelloWorld msg="Vite + Vue" />
+   <Button/>
 </template>
 
 <style scoped>
---
+++ src/components/Btn.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+
+</script>
+<template>
+    <h1>Hello Mierda</h1>
+    <button onClick="()=>{}">Hello Mierda</button>
+</template>
